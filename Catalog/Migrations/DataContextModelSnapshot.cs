aa0d4521e7b6d9e1f3af717aa6506f21ee5a6e3f
maksim-krih <maksimkrih@gmail.com>
2019-04-01 12:46:10 +0300

    Deleted second Init Migration

--- Catalog/Migrations/DataContextModelSnapshot.cs
+++ Catalog/Migrations/DataContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace Catalog.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.8-servicing-32085")
+                .HasAnnotation("ProductVersion", "2.1.4-rtm-31024")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
@@ -24,6 +24,8 @@ namespace Catalog.Migrations
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<int>("AppartmentNumber");
 
                     b.Property<string>("City");
 

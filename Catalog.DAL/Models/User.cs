5bbb2a85f4cd26b13c060e73036092b5ff5cf0b3
Ostap Tsizh <ouzhazz@gmail.com>
2019-04-16 00:41:35 +0300

    first try of adding feedbacks feature

--- Catalog.DAL/Models/User.cs
+++ Catalog.DAL/Models/User.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Catalog.DAL.Models
 {
+    [Table("User")]
     public class User 
     {
         [Key]
--- Catalog/Controllers/FacilityController.cs
+++ Catalog/Controllers/FacilityController.cs
@@ -19,13 +19,8 @@ namespace Catalog.Controllers
 
         [HttpGet]
         [Route("[controller]/{id}")]
-        public IActionResult Place (int id) //(string name)
+        public IActionResult Place (int id)
         {
-            //if (string.IsNullOrEmpty(name))
-            //    throw new ArgumentNullException();
-
-            //var facility = db.Facilities.Find(name);
-
             var facility = db.Facilities.Get(id);
 
             if(facility == null)
@@ -33,6 +28,83 @@ namespace Catalog.Controllers
 
 
             return View(facility);
+        }
+
+
+        [HttpPost]
+        public IActionResult AddFeedback(int id)
+        {
+            try
+            {
+                var facility = db.Facilities.Get(id);
+
+               
+
+                var feedback = new Feedback
+                {
+                    FacilityId = id,
+                    Message = Request.Form["Message"],
+                    Rating = Convert.ToInt32(Request.Form["Rating"]),
+                    Price = Convert.ToInt32(Request.Form["Price"]),
+                    Date = DateTime.Now,
+                    Author = db.Users.Get(Convert.ToInt32(Request.Form["AuthorId"])).Name
+                };
+                                
+
+                UpdateRating(ref facility, Convert.ToInt32(Request.Form["Rating"]));
+                UpdatePrice(ref facility, Convert.ToInt32(Request.Form["Price"]));
+                db.Save();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return RedirectToAction("Place", id);
+        }
+                
+
+        /// <summary>
+        /// Method to Update Properties of Facility
+        /// </summary>
+        /// <param name="facility">Facility that we change properties</param>
+        /// <param name="rating">field to update</param>
+        private void UpdateRating(ref Facility facility, int rating)
+        {
+            // Collection of facilities where feedbacks that were left are with rating field.
+            var query = facility.Feedbacks.Where(feedback => feedback.Rating != 0);
+
+            double sumRating = 0;
+
+            foreach (var item in query)
+            {
+                sumRating += item.Rating;
+            }
+
+            facility.Rating = sumRating / query.Count();
+            db.Save();
+        }
+
+        /// <summary>
+        /// Method to Update Properties of Facility
+        /// </summary>
+        /// <param name="facility">Facility that we change properties</param>
+        /// <param name="price">field to update</param>
+        private void UpdatePrice(ref Facility facility, int price)
+        {
+            // Collection of facilities where feedbacks that were left are with rating field.
+            var query = facility.Feedbacks.Where(feedback => feedback.Price != 0);
+
+            double sumPrice = 0;
+
+            foreach (var item in query)
+            {
+                sumPrice += item.Rating;
+            }
+
+            sumPrice += price;
+
+            facility.Rating = sumPrice / query.Count();
         }
     }
 }
